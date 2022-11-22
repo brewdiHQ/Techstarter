b710c984461c718c50dcad73e9dcbe42abdeb065
brewdiHQ <stephan.sattel@icloud.com>
2022-11-22 20:02:34 +0100

    add Antwort 8

--- uebungsaufgaben/uea_21112022/main.js
+++ uebungsaufgaben/uea_21112022/main.js
@@ -2,8 +2,8 @@
 
 // Aufgabe 5
 
-var wert1= 0;
-var wert2= 0;
+// var wert1= 0;
+// var wert2= 0;
 
 
 // function addiere(wert1 = 0, wert2 = 0){
@@ -29,3 +29,27 @@ var wert2= 0;
 
 // console.log(einkaufsliste)
 
+
+// Aufgabe 8
+
+// new Date().toLocaleString();
+// console.log(new Date)
+
+// var currentdate = new Date();
+// var datetime = "Last Sync: " + currentdate.getDay() + "/" + currentdate.getMonth() 
+// + "/" + currentdate.getFullYear() + " @ " 
+// + currentdate.getHours() + ":" 
+// + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+
+// console.log(currentdate + "Hallo")
+
+var currentdate = new Date(); 
+var datetime = "Datum: " + currentdate.getDate() + "."
+                + (currentdate.getMonth()+1)  + "." 
+                + currentdate.getFullYear() + " Uhrzeit: "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+// new Date().toLocaleString();
+console.log(datetime)
