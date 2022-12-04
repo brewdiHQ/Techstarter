6ec0d6fbc03ba4eea8a9e83d8875131837c0e2e8
brewdiHQ <stephan.sattel@icloud.com>
2022-12-04 21:39:53 +0100

    add func reset

--- uebungsaufgaben/ÜA-30-11-22/Aufgabe 3 - Stoppuhr/main.js
+++ uebungsaufgaben/ÜA-30-11-22/Aufgabe 3 - Stoppuhr/main.js
@@ -13,3 +13,16 @@ function start() {
 function stop() {
   timer = false;
 }
+
+function reset() {
+  timer = false;
+  hr = 0;
+  min = 0;
+  sec = 0;
+  count = 0;
+
+  document.getElementById("hr").innerHTML = "00";
+  document.getElementById("min").innerHTML = "00";
+  document.getElementById("sec").innerHTML = "00";
+  document.getElementById("count").innerHTML = "00";
+}
