9b49c994c22fa0e415add44fe659657417459b6b
brewdiHQ <stephan.sattel@icloud.com>
2022-12-04 21:43:54 +0100

    getElement

--- uebungsaufgaben/ÜA-30-11-22/Aufgabe 3 - Stoppuhr/main.js
+++ uebungsaufgaben/ÜA-30-11-22/Aufgabe 3 - Stoppuhr/main.js
@@ -27,21 +27,48 @@ function reset() {
   document.getElementById("count").innerHTML = "00";
 }
 
+function stopwatch() {
+  if (timer == true) {
+    count = count + 1;
 
-function stopwatch(){
-    if(timer==true){
-        count = count+1;
-    
-        if(count==100){
-            sec = sec+1;
-            count = 0;
-        }
-        if(sec==60){
-            min = min+1;
-            sec=0;
-        }
-        if(min==60){
-            hr=hr+1;
-            min=0;
-            sec=0;
-        }
+    if (count == 100) {
+      sec = sec + 1;
+      count = 0;
+    }
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
+
+    var hrstring = hr;
+    var minstring = min;
+    var secstring = sec;
+    var countstring = count;
+    if (hr < 10) {
+      hrstring = "0" + hrstring;
+    }
+    if (min < 10) {
+      minstring = "0" + minstring;
+    }
+    if (sec < 10) {
+      secstring = "0" + secstring;
+    }
+    if (count < 10) {
+      countstring = "0" + countstring;
+    }
+
+    document.getElementById("hr").innerHTML = hrstring;
+
+    document.getElementById("min").innerHTML = minstring;
+
+    document.getElementById("sec").innerHTML = secstring;
+
+    document.getElementById("count").innerHTML = countstring;
+    setTimeout("stopwatch()", 10);
+  }
+}
