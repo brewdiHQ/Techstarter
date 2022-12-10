08a0dfb3150d29b7458273d08f5b1ddbe5a23a99
Stephan Sattel <stephan.sattel@icloud.com>
2022-12-10 10:56:35 +0100

    add submodule playground

---
+++ uebungsaufgaben/ÜA-06-12-22/Aufgabe-5-Kochrezept/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Definiere ein Objekt “Kochrezept” mit den Eigenschaften Zutaten, Schwierigkeitsgrad, Dauer
+// sowie den Fähigkeiten mischen, zubereiten, anrichten;
+
+class recipe {
+    ingredients = "Eggs" "Sugar" "Flour" "Love"
+  level = "heavy";
+    duration_in_min = "45";
+    
+}
+
